9f67cbab2f9b23c56fc6bd25be4da7b6a1efeb44
ethemkocak <ethemkck1@gmail.com>
2026-04-21 14:39:56 +0300

    yeni projeler eklendi.

--- proje10/Program.cs
+++ proje10/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.Write("Metni Giriniz: ");
+                string Metin = Console.ReadLine();
+                String[] KelimeDizisi = Metin.Split(' ');
+                String Kisaltma = "";
+                foreach (String kelime in KelimeDizisi)
+                {
+                    Kisaltma += kelime[0].ToString() + ".";
+
+                }
+                Console.WriteLine("Kısaltma: " + Kisaltma.ToUpper());
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
